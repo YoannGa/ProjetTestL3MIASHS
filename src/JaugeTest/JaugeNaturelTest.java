5bf2ccb2d6908b4b8ad030f580d7203838fdc7fb
yoann.gathignol <yoann.gathignol@MI000-GS037-06.mathinfo.i-univ-tlse2.fr>
2017-10-18 15:48:27 +0200

    ok

---
+++ src/JaugeTest/JaugeNaturelTest.java
@@ -0,0 +1,59 @@
+package JaugeTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JaugeNaturelTest {
+	
+	JaugeNaturel jaj;
+	
+	@Before
+	public void setUp() throws Exception {
+		jaj = new JaugeNaturel(0, 50, 50);
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void testJaugeNaturel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstRouge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstVert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstBleu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncrementer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDecrementer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
