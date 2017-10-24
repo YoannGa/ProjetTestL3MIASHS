ed07c97fd926591d2f9c6cf9f36157b05e16fc8f
clarisse.lou <clarisse.lou@MI000-GS037-01.mathinfo.i-univ-tlse2.fr>
2017-10-24 16:44:16 +0200

    premiere version

--- src/JaugeTest/JaugeNaturelTest.java
+++ src/JaugeTest/JaugeNaturelTest.java
@@ -10,13 +10,13 @@ public class JaugeNaturelTest {
 	
 	JaugeNaturel jaV;
 	JaugeNaturel jaR;
-	JaugeNaturel jaB;
+	JaugeNaturel jaB1;
 	
 	@Before
 	public void setUp() throws Exception {
 		jaV = new JaugeNaturel(2, 0, 10);
 		jaR = new JaugeNaturel(22, 10, 20);
-		jaB = new JaugeNaturel(14, 20, 30);
+		jaB1 = new JaugeNaturel(14, 20, 30);
 	}
 
 	
